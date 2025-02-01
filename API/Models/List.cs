fbc6c740765b4f2c0228c5586cee511bf42f270b
JKentTech <contact@jkent.tech>
2025-02-02 01:24:45 +1100

    Update List.cs

--- API/Models/List.cs
+++ API/Models/List.cs
@@ -4,9 +4,15 @@ public class List
 {
     public int Id { get; }
 
+    public int OwnerId { get; set; }
+    
+    public int? SharedId { get; set; }
+
     public DateTime DateAdded { get; }
 
-    public DateTime DateModified { get; set; }
+    public DateTime? DateModified { get; set; }
 
     public string ListName { get; set; } = "new list";
+
+    public string Description { get; set; } = "list description";
 }
