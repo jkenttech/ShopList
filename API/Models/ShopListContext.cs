c45d5161485a97aa8f903054bf7404e9acc03cc4
JKentTech <contact@jkent.tech>
2025-02-09 18:22:06 +1100

    removed unnecessary default value in dbcontext

--- API/Models/ShopListContext.cs
+++ API/Models/ShopListContext.cs
@@ -4,5 +4,5 @@ namespace API.Models;
 
 internal class ShopListContext(DbContextOptions<ShopListContext> options) : DbContext(options)
 {
-    public DbSet<Login> Logins { get; set; } = default;
+    public DbSet<Login> Logins { get; set; }
 }
