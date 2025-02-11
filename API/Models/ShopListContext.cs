f3dfb3b80ddce849d60c77a5503f34cf5288939c
JKentTech <contact@jkent.tech>
2025-02-11 23:18:59 +1100

    add migrations for previous user update

--- API/Models/ShopListContext.cs
+++ API/Models/ShopListContext.cs
@@ -5,4 +5,5 @@ namespace API.Models;
 public class ShopListContext(DbContextOptions<ShopListContext> options) : DbContext(options)
 {
     public DbSet<Login> Logins { get; set; }
+    public DbSet<User> Users { get; set; }
 }
